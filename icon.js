c0ab79d4f74e288f8f9b03d9bdf77c5fcb8e5a68
shengxinjing <316783812@qq.com>
2020-04-07 10:57:57 +0800

    fix

--- icon.js
+++ icon.js
@@ -199,16 +199,23 @@ class Icon{
 		this.width = 0
         this.SIDE = 32 // favcion边长32px
         this.initCanvas()
+        this.initFilterCanvas()
 	}
 	initCanvas(){
 		this.canvas = document.createElement('canvas')
         this.canvas.width = this.canvas.height = this.SIDE
-	  }
+        // document.body.appendChild(this.canvas)
+    }
+    initFilterCanvas(){
+		this.canvasFilter = document.createElement('canvas')
+        this.canvasFilter.width = this.canvasFilter.height = this.SIDE
+        // document.body.appendChild(this.canvasFilter)
+	}
 	initVideo(url){
 		let video = document.createElement('video')
 		video.width=this.width
 		video.controls="controls"
-		video.src=url || '/ji.mp4'
+		video.src=url || '//image.shengxinjing.cn/moyu/video/ji.mp4'
 		video.crossOrigin = "anonymous"
         video.autoplay = "autoplay"
         video.volume = 0.5
@@ -259,7 +266,59 @@ class Icon{
 		context.drawImage(this.video, 0, 0, this.SIDE, this.SIDE)
 		setFavico(this.canvas)
 	}
+    videoToImageByFilter(){
+        const context = this.canvas.getContext('2d')
+		context.clearRect(0, 0, this.SIDE, this.SIDE)
+        context.drawImage(this.video, 0, 0, this.SIDE, this.SIDE)
+        
+        //获取ImageData的属性：width，height，data（包含 R G B A 四个值）；
+        var imgdata = context.getImageData(0,0,this.SIDE,this.SIDE)
+        
+        // for(var i=0;i<imgdata.data.length;i += 4){
+            // 灰色滤镜
+            //计算获取单位元素的RBG然后取平均值 然后复制给自身得到灰色的图像
+            // var avg =  (imgdata.data[i]+ imgdata.data[i+1]+ imgdata.data[i+2])/3
+            // imgdata.data[i] = imgdata.data[i+1] =imgdata.data[i+2] =avg   
 
+            // 反色滤镜
+            //将所有的RGB值重新赋值（底片效果 = 255 - 当前的RGB值）
+            // imgdata.data[i] =255-imgdata.data[i];
+            // imgdata.data[i+1] =255-imgdata.data[i+1];
+            // imgdata.data[i+2] =255-imgdata.data[i+2];
+
+            // 黑白滤镜
+            // var avg =  (imgdata.data[i]+ imgdata.data[i+1]+ imgdata.data[i+2])/3;
+            // imgdata.data[i] =avg>128 ? 255 :0;
+            // imgdata.data[i+1] =avg>128 ? 255 :0;
+            // imgdata.data[i+2] =avg>128 ? 255 :0;
+
+            //黄色滤镜的算法：红色通道值和绿色通道值增加50（红色+绿色 = 黄色）
+            // var r = imgdata.data[i] +50;
+            // var g = imgdata.data[i+1] +50
+            // //注：当前值大于255时将其赋值255
+            // imgdata.data[i] = r > 255 ? 255 : r;
+            // imgdata.data[i+1] = g > 255 ? 255 : g;
+        // }
+        // 怀旧滤镜
+        for(var i = 0; i < imgdata.height * imgdata.width; i++) {
+            var r = imgdata.data[i*4],
+                g = imgdata.data[i*4+1],
+                b = imgdata.data[i*4+2];
+        
+            var newR = (0.393 * r + 0.769 * g + 0.189 * b);
+            var newG = (0.349 * r + 0.686 * g + 0.168 * b);
+            var newB = (0.272 * r + 0.534 * g + 0.131 * b);
+            var rgbArr = [newR, newG, newB].map((e) => {
+                return e < 0 ? 0 : e > 255 ? 255 : e;
+            });
+            [imgdata.data[i*4], imgdata.data[i*4+1], imgdata.data[i*4+2]] = rgbArr;
+        }
+
+
+        //将图像信息绘制第二个canvas中，注：一般只能在服务下运行
+        this.canvasFilter.getContext('2d').putImageData(imgdata,0,0);
+		setFavico(this.canvasFilter)
+    }
     async initCam(){
         let video = document.createElement('video')
         video.width=this.width
@@ -270,10 +329,8 @@ class Icon{
         this.video.srcObject = mediaStream
 
         this.video.addEventListener('timeupdate',()=>{
-            this.videoToImage()
+            this.videoToImageByFilter()
         },false)
-
-
     }
 }
 
